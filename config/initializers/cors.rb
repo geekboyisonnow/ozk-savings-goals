134f4f4e9d5218e2ec391d2f5f47dd28166007f1
geekboyisonnow <kennethfcannon@gmail.com>
2019-03-04 09:44:55 -0500

    Changes per Gavin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
-  end
-end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins '*'
+#     resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+#   end
+# end
 
 if Rails.env.development?
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
